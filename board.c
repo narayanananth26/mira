2aa44ffebe544d0fefe60b8346851d4c8eb4345a
Ananth Narayan <narayanananth26@gmail.com>
2026-04-25 13:41:23 +0530

    feat: add material prop to board struct and create UpdateListsMaterial function

--- board.c
+++ board.c
@@ -2,6 +2,36 @@
 #include <assert.h>
 #include <stdio.h>
 
+void UpdateListsMaterial(S_BOARD *pos) {
+    int piece, sq, index, color;
+
+    for (index = 0; index < BRD_SQ_NUM; ++index) {
+        sq = index;
+        piece = pos->pieces[sq];
+
+        if (piece != OFFBOARD && piece != EMPTY) {
+            color = PieceCol[piece];
+
+            if (PieceBig[piece] == TRUE)
+                pos->bigPce[color]++;
+            if (PieceMaj[piece] == TRUE)
+                pos->majPce[color]++;
+            if (PieceMin[piece] == TRUE)
+                pos->minPce[color]++;
+
+            pos->material[color] += PieceVal[piece];
+
+            pos->pList[piece][pos->pceNum[piece]] = sq;
+            pos->pceNum[piece]++;
+
+            if (piece == wK)
+                pos->KingSq[WHITE] = sq;
+            if (piece == bK)
+                pos->KingSq[BLACK] = sq;
+        }
+    }
+}
+
 int ParseFen(char *fen, S_BOARD *pos) {
     assert(fen != NULL);
     assert(pos != NULL);
--- defs.h
+++ defs.h
@@ -16,12 +16,21 @@ typedef unsigned long long U64;
 //                 RANK_8/../RANK_1 move castlePerm enPas fiftyMove fullMoves
 #define START_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
-// clang-format off
 enum {
     EMPTY,
-    wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK,
+    wP,
+    wN,
+    wB,
+    wR,
+    wQ,
+    wK,
+    bP,
+    bN,
+    bB,
+    bR,
+    bQ,
+    bK,
 };
-// clang-format on
 
 enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE };
 
@@ -29,75 +38,131 @@ enum { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE
 
 enum { WHITE, BLACK, BOTH };
 
-// clang-format off
 enum {
-    A1 = 21, B1, C1, D1, E1, F1, G1, H1,
-    A2 = 31, B2, C2, D2, E2, F2, G2, H2,
-    A3 = 41, B3, C3, D3, E3, F3, G3, H3,
-    A4 = 51, B4, C4, D4, E4, F4, G4, H4,
-    A5 = 61, B5, C5, D5, E5, F5, G5, H5,
-    A6 = 71, B6, C6, D6, E6, F6, G6, H6,
-    A7 = 81, B7, C7, D7, E7, F7, G7, H7,
-    A8 = 91, B8, C8, D8, E8, F8, G8, H8, NO_SQ, OFFBOARD
+    A1 = 21,
+    B1,
+    C1,
+    D1,
+    E1,
+    F1,
+    G1,
+    H1,
+    A2 = 31,
+    B2,
+    C2,
+    D2,
+    E2,
+    F2,
+    G2,
+    H2,
+    A3 = 41,
+    B3,
+    C3,
+    D3,
+    E3,
+    F3,
+    G3,
+    H3,
+    A4 = 51,
+    B4,
+    C4,
+    D4,
+    E4,
+    F4,
+    G4,
+    H4,
+    A5 = 61,
+    B5,
+    C5,
+    D5,
+    E5,
+    F5,
+    G5,
+    H5,
+    A6 = 71,
+    B6,
+    C6,
+    D6,
+    E6,
+    F6,
+    G6,
+    H6,
+    A7 = 81,
+    B7,
+    C7,
+    D7,
+    E7,
+    F7,
+    G7,
+    H7,
+    A8 = 91,
+    B8,
+    C8,
+    D8,
+    E8,
+    F8,
+    G8,
+    H8,
+    NO_SQ,
+    OFFBOARD
 };
-//clang-format on
 
-enum { TRUE, FALSE};
+enum { TRUE, FALSE };
 
-enum { WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8};
+enum { WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8 };
 
-typedef struct { 
+typedef struct {
 
-  int move;
-  int castlePerm;
-  int enPas;
-  int fiftyMove;
-  U64 posKey;
-  
+    int move;
+    int castlePerm;
+    int enPas;
+    int fiftyMove;
+    U64 posKey;
+
 } S_UNDO;
 
 typedef struct {
 
-  int pieces[BRD_SQ_NUM];
-  U64 pawns[3];
-  
-  int KingSq[2];
+    int pieces[BRD_SQ_NUM];
+    U64 pawns[3];
 
-  int side; 
-  int enPas; 
-  int fiftyMove;
+    int KingSq[2];
 
-  int ply; 
-  int hisPly; 
+    int side;
+    int enPas;
+    int fiftyMove;
 
-  int castlePerm;
+    int ply;
+    int hisPly;
 
-  U64 posKey;
+    int castlePerm;
 
-  int pceNum[12];
-  int bigPce[3];
-  int majPce[3];
-  int minPce[3];
+    U64 posKey;
 
-  // piece list
-  int pList[13][10]; // type, num (max 10, when all pawns are converted)
+    int pceNum[12];
+    int bigPce[2];
+    int majPce[2];
+    int minPce[2];
+    int material[2];
 
-S_UNDO history[MAXGAMEMOVES];
+    // piece list
+    int pList[13][10]; // type, num (max 10, when all pawns are converted)
+
+    S_UNDO history[MAXGAMEMOVES];
 
 } S_BOARD;
 
-
 /* MACROS */
 
-#define FR2SQ(f, r) ( ( 21 + (f) ) + ( (r) * 10 ) )
+#define FR2SQ(f, r) ((21 + (f)) + ((r) * 10))
 #define SQ64(sq120) (Sq120ToSq64[(sq120)])
 #define SQ120(sq64) (Sq64ToSq120[(sq64)])
 #define POP(b) PopBit(b)
 #define CNT(b) CountBits(b)
-#define CLRBIT(bb, sq) ((bb) &= ClearMask[(sq)]) 
+#define CLRBIT(bb, sq) ((bb) &= ClearMask[(sq)])
 #define SETBIT(bb, sq) ((bb) |= SetMask[(sq)])
 
-/* GLOBALS */ 
+/* GLOBALS */
 
 extern int Sq120ToSq64[BRD_SQ_NUM];
 extern int Sq64ToSq120[64];
@@ -113,7 +178,11 @@ extern char SideChar[];
 extern char RankChar[];
 extern char FileChar[];
 
-
+extern int PieceBig[13];
+extern int PieceMaj[13];
+extern int PieceMin[13];
+extern int PieceVal[13];
+extern int PieceCol[13];
 
 /* FUNCTIONS */
 
@@ -132,5 +201,6 @@ extern U64 GeneratePosKey(const S_BOARD *pos);
 extern void ResetBoard(S_BOARD *pos);
 extern int ParseFen(char *fen, S_BOARD *pos);
 extern void PrintBoard(const S_BOARD *pos);
+extern void UpdateListsMaterial(S_BOARD *pos);
 
 #endif

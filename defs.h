e96d473ad418d559f3e874e0320663098c74e8de
Ananth Narayan <narayanananth26@gmail.com>
2026-04-25 06:45:58 +0530

    feat: add ResetBoard() function

--- defs.h
+++ defs.h
@@ -34,7 +34,7 @@ enum {
     A5 = 61, B5, C5, D5, E5, F5, G5, H5,
     A6 = 71, B6, C6, D6, E6, F6, G6, H6,
     A7 = 81, B7, C7, D7, E7, F7, G7, H7,
-    A8 = 91, B8, C8, D8, E8, F8, G8, H8, NO_SQ
+    A8 = 91, B8, C8, D8, E8, F8, G8, H8, NO_SQ, OFFBOARD
 };
 //clang-format on
 
@@ -57,7 +57,7 @@ typedef struct {
   int pieces[BRD_SQ_NUM];
   U64 pawns[3];
   
-  int KingSide[2];
+  int KingSq[2];
 
   int side; 
   int enPas; 
@@ -86,7 +86,8 @@ S_UNDO history[MAXGAMEMOVES];
 /* MACROS */
 
 #define FR2SQ(f, r) ( ( 21 + (f) ) + ( (r) * 10 ) )
-#define SQ64(sq120) Sq120ToSq64[sq120]
+#define SQ64(sq120) (Sq120ToSq64[(sq120)])
+#define SQ120(sq64) (Sq64ToSq120[(sq64)])
 #define POP(b) PopBit(b)
 #define CNT(b) CountBits(b)
 #define CLRBIT(bb, sq) ((bb) &= ClearMask[(sq)]) 
@@ -117,5 +118,8 @@ extern int CountBits(U64 b);
 
 // hashkeys.c
 extern U64 GenratePosKey(const S_BOARD *pos);
+
+// board.c
+extern void ResetBoard(S_BOARD *pos);
 
 #endif
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	gcc mira.c init.c bitboards.c hashkeys.c -o mira
+	gcc mira.c init.c bitboards.c hashkeys.c board.c -o mira

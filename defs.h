7e1d5044018da981a8234a2db5174f8c1bd9b5e5
Ananth Narayan <narayanananth26@gmail.com>
2026-05-01 15:11:28 +0530

    feat: create struct to hold move

--- defs.h
+++ defs.h
@@ -111,6 +111,11 @@ enum {
 enum { WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8 };
 
 typedef struct {
+    int move;
+    int score;
+} S_MOVE;
+
+typedef struct {
 
     int move;
     int castlePerm;
@@ -150,6 +155,17 @@ typedef struct {
     S_UNDO history[MAXGAMEMOVES];
 
 } S_BOARD;
+
+/* GAME MOVES */
+/*
+0000 0000 0000 0000 0000 0111 1111 -> From 0x7F
+0000 0000 0000 0011 1111 1000 0000 -> To >> 7, 0x7F
+0000 0000 0011 1100 0000 0000 0000 -> Captured >> 14, 0xF
+0000 0000 0100 0000 0000 0000 0000 -> EP 0x40000
+0000 0000 1000 0000 0000 0000 0000 -> Pawn Start 0x80000
+0000 1111 0000 0000 0000 0000 0000 -> Promoted Piece >> 20, 0xF
+0001 0000 0000 0000 0000 0000 0000 -> Castle 0x1000000
+*/
 
 /* MACROS */
 

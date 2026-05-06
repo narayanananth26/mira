225da46162680b4d50cea9f05c182be4433b21f7
Ananth Narayan <narayanananth26@gmail.com>
2026-05-06 11:11:49 +0530

    feat: create makemove.c

--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	gcc mira.c init.c bitboards.c hashkeys.c board.c data.c attack.c io.c movegen.c validate.c -o mira
+	gcc mira.c init.c bitboards.c hashkeys.c board.c data.c attack.c io.c movegen.c validate.c makemove.c -o mira
--- mira.c
+++ mira.c
@@ -5,6 +5,7 @@
 #define PAWNMOVESW "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
 #define PAWNMOVESB "rnbqkbnr/p1p1p3/3p3p/1p1p4/2P1Pp2/8/PP1P1PpP/RNBQKB1R b KQkq e3 0 1"
 #define KNIGHTSKINGS "5k2/1n6/4n3/6N1/8/3N4/8/5K2 b - - 0 1"
+#define CASTLE "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
 
 void ShowSqByAtSide(const int side, const S_BOARD *pos) {
 
@@ -32,7 +33,7 @@ int main() {
 
     S_BOARD board[1];
 
-    ParseFen(KNIGHTSKINGS, board);
+    ParseFen(CASTLE, board);
     PrintBoard(board);
 
     S_MOVELIST list[1];

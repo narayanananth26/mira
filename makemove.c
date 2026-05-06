1a1d517e9cfe7a344e84406efef185eb31ad4448
Ananth Narayan <narayanananth26@gmail.com>
2026-05-06 11:20:13 +0530

    feat: add CleerPiece function

--- makemove.c
+++ makemove.c
@@ -1,3 +1,5 @@
+#include "defs.h"
+#include <assert.h>
 
 #define HASH_PCE(pce, sq) (pos->posKey ^= (PieceKeys[(pce)][(sq)]))
 #define HASH_CA (pos->posKey ^= (CastleKeys[(pos->castlePerm)]))
@@ -22,5 +24,49 @@ const int CastlePerm[120] = {
 
 // clang-format on
 
+static void ClearPiece(const int sq, S_BOARD *pos) {
 
+    assert(SqOnBoard(sq));
+    assert(CheckBoard(pos));
 
+    int pce = pos->pieces[sq];
+
+    assert(PieceValid(pce));
+
+    int col = PieceCol[pce];
+    int index = 0;
+    int t_pceNum = -1;
+
+    assert(SideValid(col));
+
+    HASH_PCE(pce, sq);
+
+    pos->pieces[sq] = EMPTY;
+    pos->material[col] -= PieceVal[pce];
+
+    if (PieceBig[pce]) {
+        pos->bigPce[col]--;
+        if (PieceMaj[pce]) {
+            pos->majPce[col]--;
+        } else {
+            pos->minPce[col]--;
+        }
+    } else {
+        CLRBIT(pos->pawns[col], SQ64(sq));
+        CLRBIT(pos->pawns[BOTH], SQ64(sq));
+    }
+
+    for (index = 0; index < pos->pceNum[pce]; ++index) {
+        if (pos->pList[pce][index] == sq) {
+            t_pceNum = index;
+            break;
+        }
+    }
+
+    assert(t_pceNum != -1);
+    assert(t_pceNum >= 0 && t_pceNum < 10);
+
+    pos->pceNum[pce]--;
+
+    pos->pList[pce][t_pceNum] = pos->pList[pce][pos->pceNum[pce]];
+}

b7ea4316c7da7cb7ca5b38c4a348e9078e990276
Ananth Narayan <narayanananth26@gmail.com>
2026-06-13 11:54:06 +0530

    feat(search): implement ClearSearch for init

--- search.c
+++ search.c
@@ -20,16 +20,33 @@ static int IsRepetition(const S_BOARD *pos) {
 
 static void ClearForSearch(S_BOARD *pos, S_SEARCHINFO *info) {
 
+    int index = 0;
+    int index2 = 0;
+
+    for (index = 0; index < 13; ++index) {
+        for (index2 = 0; index2 < BRD_SQ_NUM; ++index2) {
+            pos->searchHistory[index][index2] = 0;
+        }
+    }
+
+    for (index = 0; index < 2; ++index) {
+        for (index2 = 0; index2 < MAXDEPTH; ++index2) {
+            pos->searchKillers[index][index2];
+        }
+    }
+
+    ClearPvTable(pos->PvTable);
+    pos->ply = 0;
+
+    info->starttime = GetTimeMs();
+    info->stopped = 0;
+    info->nodes = 0;
 }
 
 // factor in horizon-effect
-static int Quiescence(int alpha, int beta, S_BOARD *pos, S_SEARCHINFO *info) {
-    return 0;
-}
+static int Quiescence(int alpha, int beta, S_BOARD *pos, S_SEARCHINFO *info) { return 0; }
 
-static int AlphaBeta(int alpha, int beta, int depth, S_BOARD *pos, S_SEARCHINFO *info, int DoNull) {
-    return 0;
-}
+static int AlphaBeta(int alpha, int beta, int depth, S_BOARD *pos, S_SEARCHINFO *info, int DoNull) { return 0; }
 
 void SearchPosition(S_BOARD *pos, S_SEARCHINFO *info) {
     // iterative deepening, search init

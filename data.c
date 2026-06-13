236a807606b76808f2331463d8487441fdc1708b
Ananth Narayan <narayanananth26@gmail.com>
2026-06-13 11:29:17 +0530

    chore(format): cleanup formatting

--- data.c
+++ data.c
@@ -5,16 +5,19 @@ char SideChar[] = "wb-";
 char RankChar[] = "12345678";
 char FileChar[] = "abcdefgh";
 
-//                   EMPTY  P      N     B     R     Q     K     p      n     b     r     q     k
-bool PieceBig[13] = {false, false, true, true, true, true, true, false, true, true, true, true, true};
-bool PieceMaj[13] = {false, false, false, false, true, true, true, false, false, false, true, true, true};
-bool PieceMin[13] = {false, false, true, true, false, false, false, false, true, true, false, false, false};
-int PieceVal[13] = {0, 100, 325, 325, 550, 1000, 50000, 100, 325, 325, 550, 1000, 50000};
-int PieceCol[13] = {BOTH, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK};
+// clang-format off
+//                       EMPTY  P      N      B      R      Q      K      p      n      b      r      q      k
+bool PieceBig[13]      = {false, false, true,  true,  true,  true,  true,  false, true,  true,  true,  true,  true};
+bool PieceMaj[13]      = {false, false, false, false, true,  true,  true,  false, false, false, true,  true,  true};
+bool PieceMin[13]      = {false, false, true,  true,  false, false, false, false, true,  true,  false, false, false};
+int  PieceVal[13]      = {0,     100,   325,   325,   550,   1000,  50000, 100,   325,   325,   550,   1000,  50000};
+int  PieceCol[13]      = {BOTH,  WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK};
 
-bool PiecePawn[13] = {false, true, false, false, false, false, false, true, false, false, false, false, false};
-bool PieceKnight[13] = {false, false, true, false, false, false, false, false, true, false, false, false, false};
-bool PieceKing[13] = {false, false, false, false, false, false, true, false, false, false, false, false, true};
-bool PieceRookQueen[13] = {false, false, false, false, true, true, false, false, false, false, true, true, false};
-bool PieceBishopQueen[13] = {false, false, false, true, false, true, false, false, false, true, false, true, false};
-bool PieceSlides[13] = {false, false, false, true, true, true, false, false, false, true, true, true, false};
+bool PiecePawn[13]        = {false, true,  false, false, false, false, false, true,  false, false, false, false, false};
+bool PieceKnight[13]      = {false, false, true,  false, false, false, false, false, true,  false, false, false, false};
+bool PieceKing[13]        = {false, false, false, false, false, false, true,  false, false, false, false, false, true};
+bool PieceRookQueen[13]   = {false, false, false, false, true,  true,  false, false, false, false, true,  true,  false};
+bool PieceBishopQueen[13] = {false, false, false, true,  false, true,  false, false, false, true,  false, true,  false};
+bool PieceSlides[13]      = {false, false, false, true,  true,  true,  false, false, false, true,  true,  true,  false};
+
+// clang-format on

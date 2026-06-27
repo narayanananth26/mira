9d0257f913614afe1cf75ed81db13e871ca4ddea
Ananth Narayan <narayanananth26@gmail.com>
2026-06-27 18:14:32 +0530

    feat(io): add ParseSan function

--- io.c
+++ io.c
@@ -2,6 +2,7 @@
 #include "defs.h"
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
 char *PrSq(const int sq) {
 
@@ -88,6 +89,194 @@ int ParseMove(char *ptrChar, S_BOARD *pos) {
     }
 
     return NOMOVE;
+}
+
+// kingside == true  -> O-O
+// kingside == false -> O-O-O
+static int MatchCastle(S_BOARD *pos, int kingside) {
+
+    S_MOVELIST list[1];
+    GenerateAllMoves(pos, list);
+
+    int index = 0;
+    int move = 0;
+    int wantFile = kingside ? FILE_G : FILE_C;
+
+    for (index = 0; index < list->count; ++index) {
+        move = list->moves[index].move;
+
+        if (!(move & MFLAGCA))
+            continue;
+        if (FilesBrd[TOSQ(move)] != wantFile)
+            continue;
+
+        if (MakeMove(pos, move)) {
+            TakeMove(pos);
+            return move;
+        }
+    }
+
+    return NOMOVE;
+}
+
+// Standard Algebraic Notation
+// https://www.chessprogramming.org/Algebraic_Chess_Notation#Standard_Algebraic_Notation_.28SAN.29
+int ParseSan(char *ptrChar, S_BOARD *pos) {
+
+    assert(CheckBoard(pos));
+
+    char buf[16];
+    int n = 0;
+    int i = 0;
+    for (i = 0; ptrChar[i] && ptrChar[i] != '\n' && ptrChar[i] != ' ' && n < 15; ++i) {
+        char c = ptrChar[i];
+        // ignore decorations - check(+), mate(#), annotations(!/?)
+        if (c == '+' || c == '#' || c == '!' || c == '?')
+            continue;
+        buf[n++] = c;
+    }
+    buf[n] = '\0';
+    if (n == 0)
+        return NOMOVE;
+
+    // castling
+    if (strcmp(buf, "O-O-O") == 0 || strcmp(buf, "0-0-0") == 0)
+        return MatchCastle(pos, false);
+    if (strcmp(buf, "O-O") == 0 || strcmp(buf, "0-0") == 0)
+        return MatchCastle(pos, true);
+
+    // coordinate notation
+    if (n >= 4 && buf[0] >= 'a' && buf[0] <= 'h' && buf[1] >= '1' && buf[1] <= '8' && buf[2] >= 'a' && buf[2] <= 'h' && buf[3] >= '1' && buf[3] <= '8')
+        return ParseMove(buf, pos);
+
+    // SAN
+    int pieceType = wP;
+    int idx = 0;
+    switch (buf[0]) {
+    case 'N':
+        pieceType = wN;
+        idx = 1;
+        break;
+    case 'B':
+        pieceType = wB;
+        idx = 1;
+        break;
+    case 'R':
+        pieceType = wR;
+        idx = 1;
+        break;
+    case 'Q':
+        pieceType = wQ;
+        idx = 1;
+        break;
+    case 'K':
+        pieceType = wK;
+        idx = 1;
+        break;
+    default:
+        pieceType = wP;
+        idx = 0;
+        break;
+    }
+    int wantPce = (pos->side == WHITE) ? pieceType : pieceType + 6;
+
+    // [disambiguation][destination][promo]
+    char t[8];
+    int tn = 0;
+    for (i = idx; buf[i] && tn < 7; ++i) {
+        if (buf[i] == 'x' || buf[i] == '=' || buf[i] == '.')
+            continue;
+        t[tn++] = buf[i];
+    }
+    t[tn] = '\0';
+
+    // =promotion
+    int wantProm = EMPTY;
+    if (tn >= 1) {
+        int prom = EMPTY;
+        switch (t[tn - 1]) {
+        case 'N':
+            prom = wN;
+            break;
+        case 'B':
+            prom = wB;
+            break;
+        case 'R':
+            prom = wR;
+            break;
+        case 'Q':
+            prom = wQ;
+            break;
+        default:
+            break;
+        }
+        if (prom != EMPTY) {
+            wantProm = (pos->side == WHITE) ? prom : prom + 6;
+            tn--; // consume the promotion char
+        }
+    }
+
+    // destination
+    if (tn < 2)
+        return NOMOVE;
+    int destFile = t[tn - 2] - 'a';
+    int destRank = t[tn - 1] - '1';
+    if (destFile < FILE_A || destFile > FILE_H || destRank < RANK_1 || destRank > RANK_8)
+        return NOMOVE;
+    int destSq = FR2SQ(destFile, destRank);
+
+    int disFile = -1;
+    int disRank = -1;
+    for (i = 0; i < tn - 2; ++i) {
+        if (t[i] >= 'a' && t[i] <= 'h')
+            disFile = t[i] - 'a';
+        else if (t[i] >= '1' && t[i] <= '8')
+            disRank = t[i] - '1';
+    }
+
+    S_MOVELIST list[1];
+    GenerateAllMoves(pos, list);
+
+    int wantQ = (pos->side == WHITE) ? wQ : bQ;
+    int found = NOMOVE;
+    int index = 0;
+    int move = 0;
+    int from = 0;
+    int movProm = 0;
+
+    for (index = 0; index < list->count; ++index) {
+        move = list->moves[index].move;
+        from = FROMSQ(move);
+
+        if (pos->pieces[from] != wantPce)
+            continue;
+        if (TOSQ(move) != destSq)
+            continue;
+        if (disFile != -1 && FilesBrd[from] != disFile)
+            continue;
+        if (disRank != -1 && RanksBrd[from] != disRank)
+            continue;
+
+        movProm = PROMOTED(move);
+        if (wantProm != EMPTY) {
+            if (movProm != wantProm)
+                continue;
+        } else if (movProm != EMPTY) {
+            // default queen promotion
+            if (movProm != wantQ)
+                continue;
+        }
+
+        if (!MakeMove(pos, move))
+            continue;
+        TakeMove(pos);
+
+        if (found != NOMOVE)
+            return NOMOVE;
+        found = move;
+    }
+
+    return found;
 }
 
 void PrintMoveList(const S_MOVELIST *list) {
